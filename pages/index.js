5682721369218dfcb7558ca3c47d70b00238394d
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2021-09-07 16:37:01 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import QRCode from "react-qr-code";
 
 export default function Home() {
   return (
@@ -12,10 +13,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <QRCode  value="http://www.rocketseat.com.br" />
+        
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
